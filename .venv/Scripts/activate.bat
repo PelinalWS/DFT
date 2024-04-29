e457b3563cd071138f99d435beaf2bde00596f50
PelinalWS <batucan971@gmail.com>
2024-04-29 19:26:12 +0300

    Added .gitignore

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\user\Documents\VSC Projects\CMPE 362\Discrete Fourier Transform\.venv
+set VIRTUAL_ENV=C:\Users\user\Documents\VSC\CMPE 362\DFT\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
